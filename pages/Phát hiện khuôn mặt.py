4ff207f8f4558da6f3d93e84c863c344373a73ce
tuoitho <tuoithokhoc1414@gmail.com>
2024-05-06 09:09:17 +0700

    up

--- pages/Phát hiện khuôn mặt.py
+++ pages/Phát hiện khuôn mặt.py
@@ -1,7 +1,9 @@
-import streamlit as st
-import numpy as np
-import cv2 as cv
 import base64
+
+import cv2 as cv
+import numpy as np
+import streamlit as st
+
 st.set_page_config(page_title="Phát hiện khuôn mặt", page_icon="📊")
 
 st.subheader("Phát hiện khuôn mặt")
@@ -18,12 +20,13 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
+
 def add_bg_from_local(image_file):
     with open(image_file, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
 
     st.markdown(
-    f"""
+        f"""
     <style>
     .stApp {{
         background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
@@ -31,9 +34,11 @@ def add_bg_from_local(image_file):
     }}
     </style>
     """,
-    unsafe_allow_html=True
+        unsafe_allow_html=True
     )
-# add_bg_from_local('images/PhatHienKhuonMat.png')  
+
+
+add_bg_from_local('images/nhandangface.jpg')
 
 if 'stop' not in st.session_state:
     st.session_state.stop = False
@@ -64,14 +69,19 @@ def visualize(input, faces, fps, thickness=2):
             # print('Face {}, top-left coordinates: ({:.0f}, {:.0f}), box width: {:.0f}, box height {:.0f}, score: {:.2f}'.format(idx, face[0], face[1], face[2], face[3], face[-1]))
 
             coords = face[:-1].astype(np.int32)
-            cv.rectangle(input, (coords[0], coords[1]), (coords[0]+coords[2], coords[1]+coords[3]), (0, 255, 0), thickness)
+            cv.rectangle(input, (coords[0], coords[1]), (coords[0] +
+                         coords[2], coords[1]+coords[3]), (0, 255, 0), thickness)
             cv.circle(input, (coords[4], coords[5]), 2, (255, 0, 0), thickness)
             cv.circle(input, (coords[6], coords[7]), 2, (0, 0, 255), thickness)
             cv.circle(input, (coords[8], coords[9]), 2, (0, 255, 0), thickness)
-            cv.circle(input, (coords[10], coords[11]), 2, (255, 0, 255), thickness)
-            cv.circle(input, (coords[12], coords[13]), 2, (0, 255, 255), thickness)
-    cv.putText(input, 'FPS: {:.2f}'.format(fps), (1, 16), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv.circle(input, (coords[10], coords[11]),
+                      2, (255, 0, 255), thickness)
+            cv.circle(input, (coords[12], coords[13]),
+                      2, (0, 255, 255), thickness)
+    cv.putText(input, 'FPS: {:.2f}'.format(fps), (1, 16),
+               cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
     cv.imshow('Face Detection', input)
+
 
 detector = cv.FaceDetectorYN.create(
     'pages\\model\\face_detection_yunet_2023mar.onnx',
@@ -97,7 +107,7 @@ while True:
 
     # Inference
     tm.start()
-    faces = detector.detect(frame) # faces is a tuple
+    faces = detector.detect(frame)  # faces is a tuple
     tm.stop()
 
     # Draw results on the input image
